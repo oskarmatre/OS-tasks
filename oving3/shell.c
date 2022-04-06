5fea7cb6dace16e7a29876c79407e3c32bf807e4
Oskar Matre <oskarmatre@gmail.com>
2022-04-06 16:45:03 +0200

    Co-authored-by: Sebastian Hegreberg <sebasheg@stud.ntnu.no> Co-authored-by: sarapedersen <sarapedersen@users.noreply.github.com>

--- oving3/shell.c
+++ oving3/shell.c
@@ -4,6 +4,7 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
 
 int read_command ( char cmd[], char *par[] )
 {
@@ -11,12 +12,6 @@ int read_command ( char cmd[], char *par[] )
     int count = 0, i = 0, j = 0;
     char *array[100];
     char *pch;
-    // for (int i = 0; i < sizeof(*par); i++) {
-    //     //par[i] = "0";
-    //     //printf("%ld ", sizeof(*par[i]));
-    //     //memset(par[i], 0, sizeof(*par[i]));
-    //     printf("%s ", par[i]);
-    // }
     while (1) {
         int c = fgetc ( stdin );
         line[count++] = (char) c;
@@ -34,8 +29,8 @@ int read_command ( char cmd[], char *par[] )
     strcpy ( cmd, array[0] );
 
     //others are parameters
-    for ( int j = 0; j < i; j++ ) 
-        par[j] = array[j];
+    for ( int j = 0; j < i; j++ ) {
+        par[j] = array[j];}
     par[i] = NULL; //NULL-terminate the parameter list
     return i;
 }
@@ -52,114 +47,148 @@ void type_prompt()
     char path[151];
     char *cwd = getcwd(path, 150);
     
-    printf("%s > ", cwd);    //display prompt
+    printf("%s: ", cwd);    //display prompt
 }
 
+// char* in( char *param1, char *param2 ) {        // > 
+//     return NULL;
+// }
+// char* out( char *param1, char *path ) {
+//     FILE *fp;         
+//     fp = fopen(path, "w+");
+//     fprintf(fp,"%s \n",param1); // write to file 
+//     fclose(fp);
+//     return NULL;
+// }
+
+void sig_child (int sig) {
+    pid_t pid;
+    int stat;
+
+    pid = wait(&stat);
+    // if() {
+    //     kill();
+    // }
+    printf("\nChild %d is terminated\n", pid);
+    return;
+}
 
 int main() 
 {
     char cmd[100], command[100];
-    
-    //memset(parameters, 0, sizeof(parameters));
-    //parameters[20] = malloc(sizeof(char)*20);
-    
-    
-    //execl(path, NULL);
-    // char *envp[] = { (char *) "PATH=/BIN", 0 };
+    pid_t background_pid = 0;
     while ( 1 ) {
         char *parameters[20];
-        //*parameters = calloc(parameters,20);
-        //printf("%s", parameters[0]);
-        //printf("%s", parameters[1]);
-        for (int i = 0; i < 2; i++) {
-            // memset(parameters[i], '$', sizeof(parameters[i]));
+        for (int i = 0; i < 20; i++) {
              parameters[i] = (char*) malloc(sizeof(parameters[i]));
-            //printf("%s", parameters[i]);
         }
-        //printf("%s", parameters[1]);
+        //kill zombies
+        
         type_prompt();
+       
+        
+        signal(SIGCHLD, sig_child);
+        // pid_t pid_c = waitpid(-1, NULL, WNOHANG);
+        // while(!pid_c) {
+        //     pid_c = waitpid(-1, NULL, WNOHANG);
+        // }
+        // if(background_pid){
+        //     waitpid(background_pid, NULL, WNOHANG);
+
+        //     printf("%d ",background_pid);
+            
+        //     int status = kill(background_pid, SIGKILL);
+        //     printf("%d ",status);
+        //     background_pid = 0;
+        // }
+        
         int length = read_command ( command, parameters );
+        int background = (strcmp(parameters[length-2],"&"));
+
+        // printf("%d \n",background);
+
         if(strcmp(command,"cd") == 0){
             chdir(parameters[1]);
 
             }
             else{
-            //execl(command,parameters[1],NULL);
                 pid_t pid = fork();
-                // char *args[20];
-                // for(int i = 0; i<20;i++){
-                //     if(parameters[i] != NULL){
-                //         strcpy(args[i], parameters[i]);
-                //         // args[i][] = parameters[i];
-                //     }
-                // }
-                if (pid == 0) {
-                    parameters[length-1] = NULL;
-                    execv(command,parameters); // The first argument in parameters, by convention, should point to the filename associated with the file being executed
+                if (background == 0) {
+                    background_pid = pid;
                 }
-                else{
-                    wait(NULL);
+                if (pid == 0) { //Start of child
+                    int in = 0;
+                    int out = 0;
+                    // in endrer inputen og out endrer outputen
+                    for(int i = 0; i<length; i++){
+                        if(strcmp(parameters[i], "<") == 0){
+                            in = i;
+                        }
+                        if(strcmp(parameters[i], ">") == 0){
+                            out = i;
+                        }
+                    }
+        
+                    if(in){
+                        int fd0 = open(parameters[in+1], O_RDONLY);
+                        dup2(fd0, STDIN_FILENO);
+                        close(fd0);
+                    }
+                    if(out){
+                        int fd1 = creat(parameters[out + 1] , 0644) ;
+                        dup2(fd1, STDOUT_FILENO);
+                        close(fd1);
+                    }
+
+                    if(in || out){
+                        int min;
+                        if (in < out) {
+                            if (in) {
+                                min = in;
+                            }
+                            else {
+                                min = out;
+                            }
+                        }
+                        else {
+                            if(out){
+                                min = out;
+                            }
+                            else {
+                                min = in;
+                            }
+                        }
+                        //printf("%s", parameters[0]);
+                        parameters[min] = NULL;
+                    }
+                    else{
+                    parameters[length-1] = NULL;
+                    }
+                    // printf("%s", parameters[length-2]);
+
+                    if(background == 0){
+                        //printf("%s", "inneholder &\n");
+                        parameters[length-2] = NULL;
+                        execvp(command,parameters);
+                    }
+                    else{
+                        //fprintf(stderr, "Failed to exec %s\n", command);    
+                        execvp(command,parameters); // The first argument in parameters, by convention, should point to the filename associated with the file being executed
+                    }
+                    //End of Child
+                }
+                else{ //PARENT
+                    if(background != 0){ // Ingen &
+                        wait(NULL);
+                    }
+                    //inneholder & -> skal ikke vente
                 }
             }
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 20; i++) {
              free(parameters[i]);
         }
-        /*
-        pid_t pid = fork();
-        if (pid < 0)
-        {
-            printf("%s", "could not fork");
-            return 0;
-        }
-        else if(pid > 0){
-            int status;
-            if(waitpid(pid,&status,0) == -1){
-                printf("%s","failed");
-                exit(EXIT_FAILURE);
-            }
-            if(WIFEXITED(status)){
-                int es = WEXITSTATUS(status);
-                printf("%s, %d \n", "Exit status was", es);
-            }
-        }
-        else if(pid == 0){
-            if(strcmp(command,"cd") == 0){
-            chdir(parameters[1]);
-
-            }
-            else{
-            //execl(command,parameters[1],NULL);
-                execv(command,parameters); // The first argument in parameters, by convention, should point to the filename associated with the file being executed
-            }
-            exit(0);
-        }
-        else{
-            wait(NULL);
-            return 0;
-        }
-        */
-        //Kan det hende at det går galt på forken!😦🐱‍🐉🐱‍👓🐱‍🚀
-        
-        //    for (int i = 0; i < 20; i++) {
-        //     // memset(parameters[i], '$', sizeof(parameters[i]));
-        //     free(parameters[i]);
-           
-        // }
-
     }
         
-        
-        
-        // if ( fork() != 0 ) 
-        //     wait ( NULL );
-        // else {
-        //     strcpy ( cmd, "/bin/" ); //copies /bin/ to cmd
-        //     strcat ( cmd, command ); //concatinates (joins) command with cmd 
-        //     execve ( cmd, parameters, envp ); //execute command
-        
-        // if ( strcmp (command, "exit" ) == 0) 
-        //     break;
-    
 
     return 0;
 }
